18ca7047f7346276c94f789987846f9df0ab07bf
Tellybots <93035329+Tellybots@users.noreply.github.com>
2021-12-19 11:03:00 +0530

    Update about.py

--- AnonymousSenderBot/about.py
+++ AnonymousSenderBot/about.py
@@ -6,4 +6,4 @@ from pyrogram.types import InlineKeyboardMarkup
 # About Message
 @Client.on_message(filters.private & filters.command(["about"]))
 async def about(_, msg):
-	await msg.reply(text=Data.ABOUT, disable_web_page_preview=True, reply_markup=InlineKeyboardMarkup(Data.home_button))
+	await msg.reply(text=Data.ABOUT_TEXT, disable_web_page_preview=True, reply_markup=InlineKeyboardMarkup(Data.ABOUT_BUTTONS))
