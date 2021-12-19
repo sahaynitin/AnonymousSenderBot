7f614a6825ebb6c58b3e77f34608c610e8c04030
Tellybots <93035329+Tellybots@users.noreply.github.com>
2021-12-19 10:59:29 +0530

    Update Data.py

--- Data.py
+++ Data.py
@@ -1,24 +1,7 @@
 from pyrogram.types import InlineKeyboardButton
 
 
-class Data:
-    # Start Message
-    START = "Hey {}. \n\nWelcome to {} \n\nSend me anything and I'll send it back after removing the forwarded tag. \n\nBy @StarkBots ♥"
 
-    # About Message
-    ABOUT = """
-**About This Bot** 
-
-Bot created by @StarkBots
-
-Source Code : [Click Here](https://github.com/StarkBotsIndustries/AnonymousSenderBot)
-
-Framework : [Pyrogram](docs.pyrogram.org)
-
-Language : [Python](www.python.org)
-
-Developer : @StarkProgrammer
-    """
 
     # Home Button
     home_button = [[InlineKeyboardButton(text="🏠 Return Home 🏠", callback_data="home")]]
@@ -29,11 +12,4 @@ Developer : @StarkProgrammer
     # Add caption button
     add_button = [InlineKeyboardButton("💬 Re-Add Caption 💬", callback_data="add")]
 
-    # Rest Buttons
-    buttons = [
-        [
-            InlineKeyboardButton("🎪 About The Bot 🎪", callback_data="about")
-        ],
-        [InlineKeyboardButton("♥ More Amazing bots ♥", url="https://t.me/StarkBots")],
-        [InlineKeyboardButton("🎨 Support Group 🎨", url="https://t.me/StarkBotsChat")],
-    ]
+
